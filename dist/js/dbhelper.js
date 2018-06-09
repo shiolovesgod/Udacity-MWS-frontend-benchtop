a7df1127f702081cb2dadf752df033429089b050
Siobhan Williams <vonnie2k5@gmail.com>
2018-06-09 19:19:22 -0400

    chore: Modify filepaths for dev server

--- dist/js/dbhelper.js
+++ dist/js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    return `http://localhost:${port}/dist/data/restaurants.json`;
   }
 
   /**
@@ -152,7 +152,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`);
   }
 
   /**
